9e1f4bd93bd4960bbe83063376816c34b620e169
Dmitry Grechko <dmitry.grechko@deskree.com>
2020-06-10 19:58:52 -0400

    Added homework

--- Splitter/ContentView.swift
+++ Splitter/ContentView.swift
@@ -6,12 +6,22 @@
 //  Copyright © 2020 Vadym Hordov. All rights reserved.
 //
 
+// Homework #1
+
+// Adjust the following content view to include the following items inside form:
+// 1. Text with some greeting message
+// 2. 3 TextFields - for first name, last name, email (those should be states)
+// 3. Text that will display Full name
+// 4. Create an array of fishname names called "fish"
+// 5. Create picker that would allow users to select a fish from that array
+// Note: The whole view must be a Navigation view with a Large title.
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var count: Int = 0
     @State private var name: String = ""
-    @State private var greeting: String = "Hello World!"
+    var greeting: String = "Hello World!"
     let friends = ["Dima", "Vadya", "Vova"]
     @State private var selectedFriend = 0
     
