60f40f7432a0e5e79d2dcbd6d5bdf98d8a0ebed7
Dmitry Grechko <dmitry.grechko@deskree.com>
2020-06-10 19:50:39 -0400

    Added demos to ContentView

--- Splitter/ContentView.swift
+++ Splitter/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    @State private var name: String = ""
+    @State private var greeting: String = "Hello World!"
+    let friends = ["Dima", "Vadya", "Vova"]
+    @State private var selectedFriend = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Text(greeting)
+//                Two-way binding (read & write):
+                TextField("Enter your name", text: $name)
+//                One-way binding (read only):
+                Button("Tap Count \(count)") {
+                    self.count += 1
+                }
+                Picker("Select your friend", selection: $selectedFriend) {
+                    ForEach(0 ..< friends.count) {
+                       number in Text(self.friends[number])
+                    }
+                }
+                Text("Your selected friend: Friend #\(friends[selectedFriend])")
+            }
+            .navigationBarTitle("Title", displayMode: .inline)
+        }
     }
 }
 
