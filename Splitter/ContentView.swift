3f1e71de91ae99f1b96577ba2fa700d5efd49c67
Vadym Hordov <vadymhordov@gmail.com>
2020-06-10 18:54:53 -0400

    Initialized xCode Project

---
+++ Splitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Splitter
+//
+//  Created by Vadym Hordov on 2020-06-10.
+//  Copyright © 2020 Vadym Hordov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
